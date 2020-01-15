b4e4d4a95759ed5b8d4019796654f918d22db4a8
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2020-01-15 11:59:50 +0800

    Added beginTransaction, commit, and rollback function
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ module.exports = (config) => {
     },
     close: function() {
       returl util.promisify(conn.end).call(conn);
+    },
+    beginTransaction: function() {
+      return util.promisify(conn.beginTransaction)
+                .call(conn);
+    },
+    commit: function() {
+      return util.promisify(conn.commit)
+                .call(conn);
+    },
+    rollback: function() {
+      return util.promisify(conn.rollback)
+                .call(conn);
     }
   }
 };
